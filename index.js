d559a40ff7a3780b0ce567db58958bf4edc607c5
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-19 18:53:13 +0300

    fundamentals

--- index.js
+++ index.js
@@ -5,73 +5,90 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const pi=3.14159;
+printValue(pi);
 
 //TODO: ADD redeclarable variable
-
+var height=6.2;
+printValue(height);
 
 //TODO: ADD re-assignable but not redeclarable variable
-
+let age= 22;
+printValue(age);
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
-
+const number=13;
+const x=21
 
 //TODO: Create a redeclarable variable with a boolean value
-
+var isTall = true;
 
 //TODO: Create a re-assignable variable with a string value
-
-
+let name="Hello";
+let string2 ="world"
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
-
+let Concatenate =string1+ '' + string2;
 
 //TODO: Interpolate string (`${}`)
-
+let interpolatedString = `${string1} ${string2}`;
 
 //TODO: Convert to uppercase
-
+let uppercaseString = string1.toUpperCase();
 
 //TODO: Convert to lowercase
-
+let lowerrcaseString = string2.toLowerCase();
 
 //TODO: Index a specific character
-
+let indexCharacter = string1.charAt(0);
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
 
 // 4.0 COMPARISON
 //TODO: strict equality
+let number1 =10;
+let number2 =7;
 
+console.log(number1 === number10);
 
 //TODO: strict inequality
-
+console.log(number1 !== number2);
 
 //TODO: equality
-
+console.log(number1 == number2);
 
 //TODO: inequality
-
+console.log(number1 !== number2);
 
 //TODO: greater than
+let number3 = 6;
+let number4 = 9;
 
+console.log(number4 > number3);
 
 //TODO: less than
-
+console.log(number3 < number4);
 
 //TODO: greater or equal to
-
+console.log(number3 >= number4);
 
 //TODO: less or equal to
-
+console.log(number3 <= number4);
 
 
 // 5.0 CONTROL FLOW
 // TODO: if-else statement
-
+let number = 10;
+if (number > 4 ){
+    console.log('number is positive');
+} else if (number < 0) {
+    console.log('number is negative');
+} else {
+    console.log(number is 10);
+}
 
 //TODO: switch-case
 
